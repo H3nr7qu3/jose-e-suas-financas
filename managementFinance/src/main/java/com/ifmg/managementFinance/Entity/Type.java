769a1145cbd48b8c2fbe4777ca912a3de93c4200
ToninOgarotoDePrograma <tonycaldeira2007@gmail.com>
2025-04-27 00:30:09 -0300

    formataçoes no valor, e na classificação

--- managementFinance/src/main/java/com/ifmg/managementFinance/Entity/Type.java
+++ managementFinance/src/main/java/com/ifmg/managementFinance/Entity/Type.java
@@ -1,14 +1,21 @@
 package com.ifmg.managementFinance.Entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
 // Enum que categoriza as transações por tipo/gasto específico
+@AllArgsConstructor
+@Getter
 public enum Type {
-    COMPRAS,
-    ALIMENTACAO,
-    SAUDE,
-    TRANSPORTE,
-    SALARIO,
-    CONTAS,
-    INVESTIMENTO,
-    BEM_ESTAR
-    // MEGA_SENA //no futuro talvez, seu josé não tem tanta sorte
+    COMPRAS("Compras"),
+    ALIMENTACAO("Alimentação"),
+    SAUDE("Saúde"),
+    TRANSPORTE("Transporte"),
+    SALARIO("Salário"),
+    CONTAS("Contas"),
+    INVESTIMENTO("Investimento"),
+    BEM_ESTAR("Bem-estar");
+//    MEGA_SENA("Mega sena"); // um dia seu José
+
+    private final String label;
 }
